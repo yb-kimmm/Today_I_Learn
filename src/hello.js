cacb00babf6bb7fd596652f5ce611ff4f4e9dd1e
Absoyb <dydqls5757@gmail.com>
2021-02-14 19:58:08 +0900

    Class Component

--- src/hello.js
+++ src/hello.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React , {Component} from 'react';
 
-function Hello({name , color , isSpecial}){
-   return (
-      <div style={
-         {color}
-      }>
-         {isSpecial && 123}   
-         안녕하세요 {name}
-      </div>
+class Hello extends Component {
+  static defaultProps = {
+    name : '이름없음';
+  };
 
-   );
+   render (){
+      const { color , isSpecial , name} = this.props;
+      return (
+         <div style = {{color}}> 
+            {isSpecial && <b>*</b>}
+            안녕하세요 {name}
+         </div>
+      )
+   }
+
 }
+
+// function Hello({name , color , isSpecial}){
+//    return (
+//       <div style={
+//          {color}
+//       }>
+//          {isSpecial && 123}   
+//          안녕하세요 {name}
+//       </div>
+
+//    );
+// }
 
 Hello.defaultProps={
    name : "Tom"
- }
+}
 
 export default Hello;
