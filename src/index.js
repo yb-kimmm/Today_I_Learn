b062828880d5a19986f27325af7002df14c35832
Absoyb <dydqls5757@gmail.com>
2021-02-14 15:00:15 +0900

    createContext 를 이용한 전역객체

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ContextSample from './ContextSample';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
