f162cdd671337a28ae9933d47983a9a36b19387c
Absoyb <dydqls5757@gmail.com>
2021-03-02 21:16:23 +0900

    crawling use puppeteer module

---
+++ fast.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const main = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com', { waitUntil: 'networkidle2' });
+  await page.pdf({ path: 'test.pdf', format: 'A4' });
+  await browser.close();
+};
+
+main();
+
+// const main = async () => {
+//   const browser = await puppeteer.launcher();
+//   const page = await browser.newPage();
+//   await page.goto('https://google.com'); // 스크린샷 전단계
+//   await page.screenshot({ path: 'screenshot.png' });
+//   await browser.close();
+// };
+
+// main();
