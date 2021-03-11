aa15803c452b5c00736bc441bd1d776a920b89e0
nan-yb <dydqls5757@gmail.com>
2021-03-11 23:06:57 +0900

    redux-middleware - myLogger

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
+import myLogger from "./modules/myLogger";
 
-const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+const store = createStore(rootReducer, applyMiddleware(myLogger)); // 스토어를 만듭니다.
 // composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
