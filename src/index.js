e3300ba026b49e0f53f850f39443264d04c29f19
nan-yb <dydqls5757@gmail.com>
2021-03-12 22:39:23 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
-import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
-import myLogger from "./modules/myLogger";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger)); // 스토어를 만듭니다.
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+); // 스토어를 만듭니다.
 // composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 ReactDOM.render(
