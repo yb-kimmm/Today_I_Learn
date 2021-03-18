815660aa179915ddd011643bef914b7c586ea9dc
nan-yb <dydqls5757@gmail.com>
2021-03-18 21:28:35 +0900

    파이썬 공부 4일차

--- section04-3.py
+++ section04-3.py
@@ -76,3 +76,27 @@ print(y)
 
 # 삭제 : del , remove , pop
 
+# 튜플 (순서 O , 중복 O , 수정 X , 삭제 X)
+
+a = ()
+b = (1, 2, 3, 4, 5)
+c = (1, 2, 3, 4)
+d = (10, 100, ('a', 'b', 'c'))
+
+print(c[2])
+print(c[3])
+print(d[2][1])
+
+print(d[2])
+print(d[2][0:2])
+
+print(c+d)
+pront(c*3)
+
+# 튜플 함수
+z = (5, 1, 2, 4, 3)
+
+print(2)
+print(3 in z)
+print(z.index(3))
+print(z.count(1))
