b2403d43626adadbf19f786f223b7b39de54a1c1
nan-yb <dydqls5757@gmail.com>
2021-07-19 15:02:22 +0900

    node.js 20210719

---
+++ src/main.js
@@ -0,0 +1,73 @@
+// @ts-check
+
+// 프레임워크 없이 간단한 토이프로젝트 웹 서버 만들어주기
+
+/**
+ *  블로그 포스팅 서비스
+ * - 로컬 파일을 데이터베이스로 활용활 예정(JSON)
+ * - 인증 로직은 넣지 않을 예정
+ * - RESTful API를 사용합니다.
+ *
+ */
+
+const http = require("http");
+const { routes } = require("./api");
+
+const server = http.createServer((req, res) => {
+  async function main() {
+    const route = routes.find(
+      (_route) =>
+        req.url &&
+        req.method &&
+        _route.url.test(req.url) &&
+        _route.method === req.method
+    );
+
+    if (!req.url || !route) {
+      res.statusCode = 404;
+      res.end("Not found");
+      return;
+    }
+
+    const regexResult = route.url.exec(req.url);
+
+    if (!regexResult) {
+      res.statusCode = 404;
+      res.end("Not found");
+      return;
+    }
+
+    /** @type {Object.<string , *> | undefined} */
+    const reqBody =
+      (req.headers["content-type"] === "application/json" &&
+        (await new Promise((resolve, reject) => {
+          req.setEncoding("utf-8");
+          req.on("data", (data) => {
+            try {
+              resolve(JSON.parse(data));
+            } catch {
+              reject(new Error("Ill_formed json"));
+            }
+          });
+        }))) ||
+      undefined;
+
+    const result = await route.callback(regexResult, reqBody);
+    result.statusCode = result.statusCode;
+
+    if (typeof result.body === "string") {
+      res.end(result.body);
+    } else {
+      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.end(JSON.stringify(result.body));
+    }
+  }
+
+  main();
+});
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`This server is listening at port : ${PORT}`);
+});
