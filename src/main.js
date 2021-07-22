e6a54a3104ce14f25c6c2c73a520d2b0b142f9a8
nan-yb <dydqls5757@gmail.com>
2021-07-22 22:43:06 +0900

    v_2

--- src/main.js
+++ src/main.js
@@ -7,11 +7,10 @@
  * - 로컬 파일을 데이터베이스로 활용활 예정(JSON)
  * - 인증 로직은 넣지 않을 예정
  * - RESTful API를 사용합니다.
- *
  */
 
-const http = require("http");
-const { routes } = require("./api");
+const http = require('http');
+const { routes } = require('./api');
 
 const server = http.createServer((req, res) => {
   async function main() {
@@ -25,7 +24,7 @@ const server = http.createServer((req, res) => {
 
     if (!req.url || !route) {
       res.statusCode = 404;
-      res.end("Not found");
+      res.end('Not found');
       return;
     }
 
@@ -33,20 +32,20 @@ const server = http.createServer((req, res) => {
 
     if (!regexResult) {
       res.statusCode = 404;
-      res.end("Not found");
+      res.end('Not found');
       return;
     }
 
     /** @type {Object.<string , *> | undefined} */
     const reqBody =
-      (req.headers["content-type"] === "application/json" &&
+      (req.headers['content-type'] === 'application/json' &&
         (await new Promise((resolve, reject) => {
-          req.setEncoding("utf-8");
-          req.on("data", (data) => {
+          req.setEncoding('utf-8');
+          req.on('data', (data) => {
             try {
               resolve(JSON.parse(data));
             } catch {
-              reject(new Error("Ill_formed json"));
+              reject(new Error('Ill_formed json'));
             }
           });
         }))) ||
@@ -55,10 +54,10 @@ const server = http.createServer((req, res) => {
     const result = await route.callback(regexResult, reqBody);
     result.statusCode = result.statusCode;
 
-    if (typeof result.body === "string") {
+    if (typeof result.body === 'string') {
       res.end(result.body);
     } else {
-      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.setHeader('Content-Type', 'application/json; charset=utf-8');
       res.end(JSON.stringify(result.body));
     }
   }
