423fa6618a94a8bb57a3622b48db4d419b385292
nan-yb <dydqls5757@gmail.com>
2021-07-25 22:20:08 +0900

    20210725 stream-1

---
+++ src/20210725/main.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+// const os = require('os');
+
+// console.log(
+//   ['arch', os.arch()],
+//   ['platform ', os.platform()],
+//   ['cpus', os.cpus()]
+// );
+
+// const dns = require('dns');
+
+// dns.lookup('google.com', (err, address, family) => {
+//   console.log(`google.com address :  ${address} `);
+// });
+
+//
+// const path = require('path');
+// const fs = require('fs');
+
+// const filePath = path.resolve(__dirname, './fileTxt.txt');
+// const fileContent = fs.readFileSync(filePath, 'utf-8');
+
+// console.log(fileContent);
