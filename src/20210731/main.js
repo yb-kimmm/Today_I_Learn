fc5fae6fa755f309f13504d6da2f2996baeabdbc
nan-yb <dydqls5757@gmail.com>
2021-08-01 17:15:03 +0900

    expree middleware

--- src/20210731/main.js
+++ src/20210731/main.js
@@ -1,14 +1,31 @@
 // @ts-check
 
 const express = require('express');
-
 const app = express();
+const PORT = 5000;
+const fs = require('fs');
 
-app.use('/', (req, res) => {
-  res.send('Hello , express!');
+app.use('/', async (req, res, next) => {
+  const requestAt = new Date();
+  console.log('Middleware 1');
+
+  const fileContent = await fs.promises.readFile('.gitignore');
+
+  // @ts-ignore
+  req.requestAt = requestAt;
+  req.fileContent = fileContent;
+  next();
 });
 
-const PORT = 5000;
+app.use((req, res) => {
+  console.log('Middleware 2 ');
+
+  // @ts-check
+  res.send(
+    `Hello , express!: Request at ${req.requestAt} , ${req.fileContent}`
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`The Express server is listening at port : ${PORT}`);
 });
