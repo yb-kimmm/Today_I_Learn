6f9e9d5de5ab923dae2af4dedefe423080648eca
nan-yb <dydqls5757@gmail.com>
2021-08-03 21:59:12 +0900

    mongo db -2

--- src/main.js
+++ src/main.js
@@ -10,8 +10,15 @@ async function main() {
   const c = await client.connect();
 
   const users = client.db('fc21').collection('users');
+  const cities = client.db('fc21').collection('cities');
 
   await users.deleteMany({});
+  await cities.deleteMany({});
+
+  await cities.insertMany([
+    { name: '서울', population: 1000 },
+    { name: '대전', population: 500 },
+  ]);
 
   await users.insertMany([
     {
@@ -22,12 +29,17 @@ async function main() {
           type: 'phone',
           number: '+8201012345678',
         },
+        {
+          type: 'tel',
+          number: '+1234567',
+        },
       ],
     },
 
     {
       name: 'Bar',
       birthYear: 1995,
+      city: '서울',
       contacts: [
         {
           type: 'tel',
@@ -38,6 +50,7 @@ async function main() {
     {
       name: 'Baz',
       birthYear: 1990,
+      city: '대전',
       contacts: [
         {
           type: 'phone',
@@ -48,27 +61,58 @@ async function main() {
     {
       name: 'Poo',
       birthYear: 1993,
+      city: '대전',
     },
   ]);
 
   // await users.updateOne({ name: 'Baz' }, { $set: { name: 'Boo' } });
 
-  await users.deleteOne({
-    name: 'Baz',
-  });
+  // await users.deleteOne({
+  //   name: 'Baz',
+  // });
 
-  const cursor = users.find(
+  // const cursor = users.find(
+  //   {
+  //     birthYear: {
+  //       $gte: 1990,
+  //     },
+  //   },
+  //   {
+  //     sort: {
+  //       birthYear: -1,
+  //     },
+  //   }
+  // );
+  const cursor = users.aggregate([
     {
-      birthYear: {
-        $gte: 1990,
+      $lookup: {
+        from: 'cities',
+        localField: 'city',
+        foreignField: 'name',
+        as: 'city_info',
       },
     },
     {
-      sort: {
-        birthYear: -1,
+      $match: {
+        $and: [
+          {
+            'city_info.population': {
+              $gte: 500,
+            },
+          },
+          {
+            birthYear: {
+              $gte: 1995,
+            },
+          },
+        ],
       },
-    }
-  );
+    },
+    // {
+    //   $count: 'num_users',
+    // },
+  ]);
+
   await cursor.forEach(console.log);
 
   await client.close();
