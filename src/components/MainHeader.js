157c10ddb73483977e66fb6962db55e7c98b273c
nan-yb <dydqls5757@gmail.com>
2021-09-06 17:51:03 +0900

    useREf

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function MainHeader() {
-  const [text, setText] = useState('Hello');
+  const [name, setName] = useState({
+    first: '상훈',
+    last: '한',
+  });
 
-  const onChange = e => {
-    const inputText = e.target.value;
-    console.log(e.target);
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
 
-    setText(inputText);
+  const { first, last } = name;
+
+  const confirm = () => {
+    setName({
+      first: firstNameRef.current.value,
+      last: lastNameRef.current.value,
+    });
   };
 
   return (
     <div classname="App">
-      <h1>{text}</h1>
-      <input onChange={onChange}></input>
+      <h1>
+        {last}
+        {first}
+      </h1>
+      <input name="성" placeholder="성" ref={lastNameRef}></input>
+      <input name="이름" placeholder="이름" ref={firstNameRef}></input>
+      <input type="button" value="확인" onClick={confirm}></input>
     </div>
   );
 }
