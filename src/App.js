658f90d020ed3fb0f5d6e243d946971a688f41e2
nan-yb <dydqls5757@gmail.com>
2021-09-10 22:19:39 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
-import MainHeader from './components/MainHeader';
-import CustomList from './components/CustomList';
 
 function App() {
-  const array1 = ['apple', 'banana', 'orange'];
-  return (
-    <div className="App">
-      <MainHeader></MainHeader>
-      <ul>
-        {array1.map(v => {
-          return <CustomList text={v}></CustomList>;
-        })}
-      </ul>
-    </div>
-  );
+  return <div>123</div>;
 }
 
 export default App;
