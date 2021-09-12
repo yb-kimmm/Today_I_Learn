71a3488a958f56be3cb78e4960acd68c40644de4
nan-yb <dydqls5757@gmail.com>
2021-09-12 14:56:23 +0900

    quiz

--- src/store/modules/score.js
+++ src/store/modules/score.js
@@ -1,27 +1,26 @@
 // 퀴즈를 풀면 정답 -> 점수 획득
 // 오답 -> 점수 획득 X
 
-// 상태 : 사용자의 최종 점수(score)
-// 액션 : 정답인지 아닌지 판별 , 정답 점수++;
+// 상태: 사용자의 현재 점수(score)
+// 액션: 정답인지 아닌지 판별, 정답 => 점수++;
 
-// 상태 : 사용자가 응답한 정답 목록 , [0 , 2, 1]
-// 액션 : 정답 목록에 하나하나 추가
-
-// 액션 타입 ( 문자열 )
+// 액션 타입(문자열)
 const CHECK_CORRECT = 'score/CHECK_CORRECT';
-const NEXT_PAGE = 'score/NEXTPAGE';
+const NEXT_PAGE = 'score/NEXT_PAGE';
 const RESET = 'score/RESET';
 
 // 액션 생성 함수
-export function check({ quizIndex, answerIndex }) {
+export function check({ isCorrect }) {
   return {
     type: CHECK_CORRECT,
-    payload: { quizIndex, answerIndex },
+    payload: { isCorrect },
   };
 }
 
 export function next() {
-  return { type: NEXT_PAGE };
+  return {
+    type: NEXT_PAGE,
+  };
 }
 
 export function reset() {
@@ -33,36 +32,163 @@ export function reset() {
 // 초기 상태
 const initialState = {
   score: 0,
-  page: 0, // 0: dㅣㄴ트로 페이지 , 1~ quize.length : 퀴즈 페이지 , quize.length+1: 마지막 페이지
+  page: 0, // 0: 인트로 페이지, 1 ~ quizs.length: 퀴즈 페이지, quizs.length + 1: 마지막 페이지
   quizs: [
     {
       q: '대한민국의 수도는?',
+      img: '/city/korea.jpg',
       a: [
         {
-          test: '서울',
+          text: '서울',
           isCorrect: true,
         },
         {
-          test: '인천',
+          text: '인천',
           isCorrect: false,
         },
         {
-          test: '부선',
+          text: '부산',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      q: '미국의 수도는?',
+      img: '/city/us.jpg',
+      a: [
+        {
+          text: '샌프란시스코',
+          isCorrect: false,
+        },
+        {
+          text: '워싱턴 D.C',
+          isCorrect: true,
+        },
+        {
+          text: '뉴욕',
           isCorrect: false,
         },
       ],
     },
   ],
 };
+
 // 리듀서
 export default function score(state = initialState, action) {
+  console.log(action);
   switch (action.type) {
     case CHECK_CORRECT:
       return {
         ...state,
-        score: state.quizs[action.payload.quizIndex].isCorrect
-          ? score + 10
-          : score,
+        score: action.payload.isCorrect ? state.score + 10 : state.score,
       };
     case NEXT_PAGE:
       return {
