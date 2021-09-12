e28717ec4c36ca9093f69ef9f22bb4396fec594c
nan-yb <dydqls5757@gmail.com>
2021-09-12 20:09:51 +0900

    vue start

---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template lang="">
+  <div>
+    <div>
+      <input type="text" v-model="inputValue"/>
+      <button @click="addItem">추가하기</button>
+      <ul>
+        <ListItem v-for="(value , index) in listArray" :key="index" style="style" :text="value" :index="index" @delete="catchDeleteItem"/>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import ListItem from "./ListItem.vue"
+
+export default {
+  data() {
+    return {
+      inputValue : "",
+      listArray :[] ,
+      style :{
+        color :"red"
+      }
+    }
+  },
+  components:{
+    ListItem
+  } ,
+  methods : {
+    addItem(){
+      this.listArray.push(this.inputValue)
+      this.inputValue = ""
+    } ,
+    catchDeleteItem(index){
+      this.listArray.splice(index,1)
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
