ff4b8c7d96f7ed222b8af2afbb8f8716cf4c0343
nan-yb <dydqls5757@gmail.com>
2021-09-14 19:18:55 +0900

    project clear

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <TodoList />
   </div>
 </template>
 
 <script>
-import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
   components: {
-    TodoList
   }
 }
 </script>
