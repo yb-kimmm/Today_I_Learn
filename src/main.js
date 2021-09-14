5f38a666f283286dffc33118165212fa301d9a50
nan-yb <dydqls5757@gmail.com>
2021-09-14 20:10:46 +0900

    vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+const store = new Vuex.store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
+
+store.commit('increment');
+console.log(store.state.count);
 
 new Vue({
   router,
