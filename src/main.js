4b11077d1bc3005e80d89c41d680d1eccfafa099
nan-yb <dydqls5757@gmail.com>
2021-09-15 14:30:40 +0900

    project clear

--- src/main.js
+++ src/main.js
@@ -1,26 +1,6 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import Vuex from 'vuex';
-
-Vue.config.productionTip = false;
-Vue.use(Vuex);
-
-const store = new Vuex.store({
-  state: {
-    count: 0,
-  },
-  mutations: {
-    increment(state) {
-      state.count++;
-    },
-  },
-});
-
-store.commit('increment');
-console.log(store.state.count);
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
