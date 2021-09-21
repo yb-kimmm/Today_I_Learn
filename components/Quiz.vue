cf5227cc0f0852f36a22dae33c139457e760a675
nan-yb <dydqls5757@gmail.com>
2021-09-21 23:12:47 +0900

    nuxt tutorial

---
+++ components/Quiz.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <div>
+    <h1></h1>
+    <Button styleType="blue" />
+    <Progress />
+  </div>
+</template>
+<script>
+export default {
+  computed:{
+
+  },
+  methods :{
+
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <main v-if="page===0">
+    <img src="" alt="원피스 캐릭터들">
+    <h1>원피스 MBTI</h1>
+    <h1>나와 닮은 원피스 캐릭터 찾기!</h1>
+    <h2>나와 같은 원피스 캐릭터는 누구일까요?</h2>
+    <Button text="테스트 시작!" />
+  </main>
+
+<!-- 퀴즈페이지 -->
+  <main v-else>
+  </main>
+</template>
+
+<script>
+export default {
+  computed : {
+    page(){
+      return 0;
+    },
+    quiz(){
+      return [];
+    }
+  }
+}
+</script>
+<style>
+main {
+  padding : 30px 0;
+  width : 100%;
+  max-width : 360px;
+  margin : auto;
+  text-align : center;
+}
+
+h1 {
+  font-size : 1.8em;
+}
+
+h2{
+  font-size: 1.2em;
+  color : #b9b8b7;
+  margin-bottom : 25px
+}
+</style>
