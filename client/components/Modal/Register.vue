707dda474ebdffcb3bcdd523dc45196e1f572748
nan-yb <dydqls5757@gmail.com>
2021-11-30 17:04:45 +0900

    20211130-tailwind css intall

--- client/components/Modal/Register.vue
+++ client/components/Modal/Register.vue
@@ -1,16 +1,98 @@
 <template>
-    <div  class="body">
-      <div class="row">
-        <label for="user-email">이메일</label>
-        <input id="user-email" type="email" v-model="email" />
+  <div v-if="modal.login.show" class="modal-outside">
+    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <a @click.prevent="$store.commit('modal/SET_LOGIN_MODAL_CLOSE')" class="close-btn">
+        <img src="/icon/close.png" alt="닫기" />
+      </a>
+      <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div class="max-w-md mx-auto">
+            <div>
+              <img src="/img/logo.svg" class="h-7 sm:h-8" />
+            </div>
+            <div class="divide-y divide-gray-200">
+              <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+                <ul class="list-disc space-y-2">
+                  <li class="flex items-start">
+                    <span class="h-6 flex items-center sm:h-7">
+                      <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                      </svg>
+                    </span>
+                    <p class="ml-2">
+                      Customizing your
+                      <code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+                    </p>
+                  </li>
+                  <li class="flex items-start">
+                    <span class="h-6 flex items-center sm:h-7">
+                      <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                      </svg>
+                    </span>
+                    <p class="ml-2">
+                      Extracting classes with
+                      <code class="text-sm font-bold text-gray-900">@apply</code>
+                    </p>
+                  </li>
+                  <li class="flex items-start">
+                    <span class="h-6 flex items-center sm:h-7">
+                      <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                      </svg>
+                    </span>
+                    <p class="ml-2">Code completion with instant preview</p>
+                  </li>
+                </ul>
+                <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+              </div>
+              <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                <p>Want to dig deeper into Tailwind?</p>
+                <p>
+                  <a href="https://tailwindcss.com/docs" class="text-cyan-600 hover:text-cyan-700"> Read the docs &rarr; </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="row">
-        <label for="user-password">비밀번호</label>
-        <input id="user-password" type="password" v-model="password" />
-      </div>
-      <button class="login-btn" @click="loginWithEmail">이메일로 로그인</button>
     </div>
+  </div>
 </template>
+
+<script>
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
+export default {
+  setup() {
+    return {
+      people,
+    }
+  },
+}
+</script>
+
 <script>
 import { mapState } from "vuex";
 
@@ -51,76 +133,6 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
-#login-modal {
-  background: white;
-  width: 520px;
-  .body {
-    font-size: 16px;
-    padding: 0 30px;
-    line-height: 24px;
-    p {
-      padding: 20px 0;
-      margin: 0;
-    }
-    .info {
-      color: rgb(148, 150, 155);
-      font-size: 14px;
-      letter-spacing: -0.1px;
-      margin: 20px 0 30px;
-      line-height: 21px;
-    }
-    .row {
-      margin: 20px 0;
-      label {
-        display: block;
-      }
-      input {
-        width: 100%;
-        box-sizing: border-box;
-        padding: 12px;
-      }
-    }
-    .otp-btn {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: rgb(55, 172, 201);
-      border: none;
-      color: white;
-      font-size: 40px;
-      font-weight: 400;
-      width: 100%;
-      height: 82px;
-    }
-    .login-btn {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: rgb(55, 172, 201);
-      border: none;
-      color: white;
-      font-size: 24px;
-      font-weight: 400;
-      width: 100%;
-      height: 64px;
-      margin-bottom: 30px;
-    }
-    .left-time {
-      text-align: center;
-      color: rgb(55, 172, 201);
-      font-size: 14px;
-      font-weight: 700;
-      margin-top: 16px;
-    }
-  }
-  .foot {
-    color: rgb(160, 160, 174);
-    font-size: 14px;
-    line-height: 17.5px;
-    text-align: center;
-    text-decoration: underline;
-    padding: 30px;
-  }
-}
-</style>
+<!-->
+
+
