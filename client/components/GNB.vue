c00fbfffd06e5fd0aff7648163dc8dc850aa931e
nan-yb <dydqls5757@gmail.com>
2021-12-04 21:45:42 +0900

    20211204

--- client/components/GNB.vue
+++ client/components/GNB.vue
@@ -6,15 +6,15 @@
           <img src="/logo/main.png" alt="블라인드 로고" width="150" />
         </nuxt-link>
         <nuxt-link to="/" :class="['text-menu', $route.name === 'index' && 'active']">홈</nuxt-link>
-        <nuxt-link to="/write" :class="['text-menu', $route.name === 'write' && 'active']">글쓰기</nuxt-link>
+        
       </div>
       <div class="side-block">
         <!-- <SmallSearchbar /> -->
-        <button @click.prevent="clickWritingButton"  class="hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2">
+        <button class="hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2">
           <svg class="group-hover:text-light-blue-600 text-light-blue-500 mr-2" width="12" height="20" fill="currentColor">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"/>
           </svg>
-          글쓰기
+          <nuxt-link to="/write" :class="[$route.name === 'write']">글쓰기</nuxt-link>
         </button>
 
         <button @click.prevent="clickLoginButton"  class="hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2">
