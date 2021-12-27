408cbd03056edbbff0b5d83895d0f3236afaf29a
nan-yb <dydqls5757@gmail.com>
2021-12-27 21:09:53 +0900

    20211217-메인 리스트

--- react-client/src/components/Home.js
+++ react-client/src/components/Home.js
@@ -1,6 +1,33 @@
 import React from "react";
+import axios from "../api";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [boardList, setBoardList] = useState([]);
+  console.log(boardList);
+
+  const fetchHotels = async () => {
+    try {
+      // 요청 처음에 초기화
+      setBoardList([]);
+
+      const data = await axios({
+        url: "/main",
+        method: "get",
+      });
+
+      console.log(data.content[0].content);
+
+      setBoardList(data);
+    } catch (e) {
+      console.err(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchHotels();
+  }, []);
+
   return (
     <div>
       <div className="container mx-auto my-4 max-w-screen-xl">
@@ -9,6 +36,12 @@ function Home() {
           alt=""
           className="mx-auto justify-center items-end pb-4 flex rounded-3xl "
         />
+      </div>
+      <div>
+        {/* {boardList.map(
+          (board) => console.log(board)
+          // <div>{board.title}</div>
+        )} */}
       </div>
     </div>
   );
