3f4e3b29afee354f7f3103becaf9f8b11d79078f
nan-yb <dydqls5757@gmail.com>
2022-01-08 23:14:42 +0900

    게시판 글 가져오기 O - CSS 작업 필요

--- react-client/src/components/Article.js
+++ react-client/src/components/Article.js
@@ -1,25 +1,64 @@
 import { useParams  } from "react-router-dom";
 import axios from "../api";
-
-async function getArticle(articleId){
-  const data = await axios({
-    url: `/article/1`,
-    method: "get",
-  });
-
-  console.log(data)
-}
+import { Viewer } from "@toast-ui/react-editor";
+import { useRef , useEffect , useState} from "react";
 
 const Article = () => {
-
-  let params = useParams();
-
-  getArticle(params.articleId);
-
+  const params = useParams();
   
+  const [title, setTitle] = useState();
+
+  const editorRef = useRef();
+  const fetchArticles = async () => {
+    try {
+      // 요청 처음에 초기화
+      var url = `/article/${params.article}`;
+
+      const data = await axios({
+        url: url,
+        method: "get",
+      });
+
+      setTitle(data.article.board.title)
+
+      if(data.article){
+        editorRef.current.getInstance().setMarkdown(`${data.article.content}`)
+      }
+      
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    fetchArticles();
+    
+  }, []);
+
   return (
     <div>
-      params : {params.article}
+      <div className="mx-auto max-w-6xl py-10 h-full">
+        <div className="py-4">
+          <button className="flex">
+            <span></span>
+          </button>
+
+          <div className="md:flex md:items-center mb-6">
+            <div className="">
+              <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4">
+                제목
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <span className="p-4 w-80 h-10 rounded text-lg text-gray-500 font-bold ">
+                {title}
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <Viewer 
+          ref={editorRef}
+        />        
+      </div>
     </div>
   )
 }
