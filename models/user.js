5ba322e4dc4dd26661fd3844a09e7809e28d452d
nan-yb <dydqls5757@gmail.com>
2022-03-02 18:51:31 +0900

    [노드교과서]-테스트

--- models/user.js
+++ models/user.js
@@ -2,42 +2,39 @@ const Sequelize = require('sequelize');
 
 module.exports = class User extends Sequelize.Model {
   static init(sequelize) {
-    return super.init(
-      {
-        email: {
-          type: Sequelize.STRING(40),
-          allowNull: true,
-          unique: true,
-        },
-        nick: {
-          type: Sequelize.STRING(15),
-          allowNull: false,
-        },
-        password: {
-          type: Sequelize.STRING(100),
-          allowNull: true,
-        },
-        provider: {
-          type: Sequelize.STRING(10),
-          allowNull: false,
-          defaultValue: 'local',
-        },
-        snsId: {
-          type: Sequelize.STRING(30),
-          allowNull: true,
-        },
+    return super.init({
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: true,
+        unique: true,
       },
-      {
-        sequelize,
-        timestamps: true,
-        underscored: false,
-        modelName: 'User',
-        tableName: 'users',
-        paranoid: true,
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-      }
-    );
+      nick: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      provider: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        defaultValue: 'local',
+      },
+      snsId: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
   }
 
   static associate(db) {
@@ -52,6 +49,5 @@ module.exports = class User extends Sequelize.Model {
       as: 'Followings',
       through: 'Follow',
     });
-    db.User.hasMany(db.Domain);
   }
 };
--- passport/index.js
+++ passport/index.js
@@ -11,21 +11,18 @@ module.exports = () => {
   passport.deserializeUser((id, done) => {
     User.findOne({
       where: { id },
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'nick'],
-          as: 'Followers',
-        },
-        {
-          model: User,
-          attributes: ['id', 'nick'],
-          as: 'Followings',
-        },
-      ],
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
     })
-      .then((user) => done(null, user))
-      .catch((err) => done(err));
+      .then(user => done(null, user))
+      .catch(err => done(err));
   });
 
   local();
