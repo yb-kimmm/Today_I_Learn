e5fdf2ba4caa3aa51c9376750c17b6ce79e23d86
nan-yb <dydqls5757@gmail.com>
2022-03-07 21:23:45 +0900

    [feat]-NODE교과서-경매장

--- node/nodebook/20220307/NodeAuction/app.js
+++ node/nodebook/20220307/NodeAuction/app.js
@@ -3,23 +3,37 @@ const path = require("path");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const passport = rqeuire("passport");
+const sse = require("./sse");
+const webSocket = require("./socket");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
-const ColorHash = require("color-hash");
 
 dotenv.config();
-const webSocket = require("./socket");
-const indexRouter = require("./routes");
-const connect = require("./schemas");
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
+const { noExtendRight } = require("sequelize/types/lib/operators");
 
 const app = express();
-app.set("port", process.env.PORT || 8005);
+passportConfig();
+app.set("port", process.env.PORT || 8010);
 app.set("view engine", "html");
+nunjucks.configure("views", "html");
 nunjucks.configure("views", {
   express: app,
   watch: true,
 });
-connect();
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터 베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const sessionMiddleware = session({
   resave: false,
@@ -30,23 +44,19 @@ const sessionMiddleware = session({
     secure: false,
   },
 });
+
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/gif", express.static(path.join(__dirname, "uploads")));
+app.use("/img", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(sessionMiddleware);
-
-app.use((req, res, next) => {
-  if (!req.session.color) {
-    const colorHash = new ColorHash();
-    req.session.color = colorHash.hex(req.sessionID);
-  }
-  next();
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -61,8 +71,6 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
-const server = app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중");
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
 });
-
-webSocket(server, app, sessionMiddleware);
