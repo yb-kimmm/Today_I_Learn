0de552f8bcd61fbec75bb7fa6f6068a50c1c31cd
nan-yb <dydqls5757@gmail.com>
2022-03-08 18:10:56 +0900

    [feat]-Node교과서-NodeAuction

--- node/nodebook/20220307/NodeAuction/routes/index.js
+++ node/nodebook/20220307/NodeAuction/routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const schedule = require("node-schedule");
 
-const { Good, Auction, User } = require('../models');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Good, Auction, User, sequelize } = require("../models");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
@@ -13,11 +14,11 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const goods = await Good.findAll({ where: { SoldId: null } });
-    res.render('main', {
-      title: 'NodeAuction',
+    res.render("main", {
+      title: "NodeAuction",
       goods,
     });
   } catch (error) {
@@ -26,47 +27,139 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/join', isNotLoggedIn, (req, res) => {
-  res.render('join', {
-    title: '회원가입 - NodeAuction',
+router.get("/join", isNotLoggedIn, (req, res) => {
+  res.render("join", {
+    title: "회원가입 - NodeAuction",
   });
 });
 
-router.get('/good', isLoggedIn, (req, res) => {
-  res.render('good', { title: '상품 등록 - NodeAuction' });
+router.get("/good", isLoggedIn, (req, res) => {
+  res.render("good", { title: "상품 등록 - NodeAuction" });
 });
 
 try {
-  fs.readdirSync('uploads');
+  fs.readdirSync("uploads");
 } catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
 }
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, cb) {
-      cb(null, 'uploads/');
+      cb(null, "uploads/");
     },
     filename(req, file, cb) {
       const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
+      cb(
+        null,
+        path.basename(file.originalname, ext) + new Date().valueOf() + ext
+      );
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
-router.post('/good', isLoggedIn, upload.single('img'), async (req, res, next) => {
+router.post(
+  "/good",
+  isLoggedIn,
+  upload.single("img"),
+  async (req, res, next) => {
+    try {
+      const { name, price } = req.body;
+      await Good.create({
+        OwnerId: req.user.id,
+        name,
+        img: req.file.filename,
+        price,
+      });
+
+      const end = new Date();
+      end.setDate(end.getDate() + 1);
+      schedule.scheduleJob(end, async () => {
+        const success = await Aution.findOne({
+          where: { GoodId: good.id },
+          order: [["bid", "DESC"]],
+        });
+      });
+
+      await Good.update(
+        { SoldId: success.User.id },
+        { where: { id: good.id } }
+      );
+      await User.update(
+        { money: sequelize.literal(`money-${success.bid}`) },
+        {
+          where: { id: success.UserId },
+        }
+      ); // 문자로 변경
+
+      res.redirect("/");
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  }
+);
+
+router.get("/good/:id", isLoggedIn, async (req, res, next) => {
   try {
-    const { name, price } = req.body;
-    await Good.create({
-      OwnerId: req.user.id,
-      name,
-      img: req.file.filename,
-      price,
+    const [good, auction] = await Promise.all([
+      Good.findOne({
+        where: { id: req.params.id },
+        include: {
+          model: User,
+          as: "Owner",
+        },
+      }),
+      Auction.findAll({
+        where: { GoodId: req.params.id },
+        include: { model: User },
+        order: [["bid", "ASC"]],
+      }),
+    ]);
+    res.render("auction", {
+      title: `${good.name} - NodeAuction`,
+      good,
+      auction,
     });
-    res.redirect('/');
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+router.post("/good/:id/bid", isLoggedIn, async (req, res, next) => {
+  try {
+    const { bid, msg } = req.body;
+    const good = await Good.findOne({
+      where: { id: req.params.id },
+      include: { model: Auction },
+      order: [[{ model: Auction }, "bid", "DESC"]],
+    });
+    if (good.price >= bid) {
+      return res.status(403).send("시작 가격보다 높게 입찰해야 합니다.");
+    }
+    if (new Date(good.createdAt).valueOf() + 24 * 60 * 60 * 1000 < new Date()) {
+      return res.status(403).send("경매가 이미 종료되었습니다");
+    }
+    if (good.Auctions[0] && good.Auctions[0].bid >= bid) {
+      return res.status(403).send("이전 입찰가보다 높아야 합니다");
+    }
+    const result = await Auction.create({
+      bid,
+      msg,
+      UserId: req.user.id,
+      GoodId: req.params.id,
+    });
+    // 실시간으로 입찰 내역 전송
+    req.app.get("io").to(req.params.id).emit("bid", {
+      bid: result.bid,
+      msg: result.msg,
+      nick: req.user.nick,
+    });
+    return res.send("ok");
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 
