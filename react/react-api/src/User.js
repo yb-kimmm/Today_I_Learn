dc4b5d651297a1a5bca1d0b2be8b4239fbcfc35e
nan-yb <dydqls5757@gmail.com>
2022-04-07 21:36:49 +0900

    [study]-refactoring

--- react/react-api/src/User.js
+++ react/react-api/src/User.js
@@ -1,22 +1,17 @@
-import React from "react";
-import axios from "axios";
-import { useAsync } from "react-async";
-
-async function getUser({ id }) {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-}
+import React, { useEffect } from "react";
+import { getUser, useUsersDispatch, useUsersState } from "./UsersContext";
 
 function User({ id }) {
-  const {
-    data: user,
-    error,
-    isLoading,
-  } = useAsync({ promiseFn: getUser, id, watch: id });
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
 
-  if (isLoading) return <div>로딩중..</div>;
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
+
+  const { loading, data: user, error } = state.user;
+
+  if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
