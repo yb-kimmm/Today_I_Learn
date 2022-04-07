13004baab52da39ae7115678db4ee70f067eed70
nan-yb <dydqls5757@gmail.com>
2022-04-07 19:03:48 +0900

    [study]-useReducer를 이용한 API관리

--- react/react-api/src/Users.js
+++ react/react-api/src/Users.js
@@ -1,40 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import axios from "axios";
 
-function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+// LOADING , SUCCESS , ERROR
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type : ${action.type} `);
+  }
+}
 
+function Users() {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
+
     try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/users/"
       );
-
-      setUsers(response.data);
+      dispatch({ type: "SUCCESS", data: response.data });
     } catch (error) {
-      setError(error);
+      dispatch({ type: "ERROR", error: error });
     }
-
-    setLoading(false);
   };
+
+  const { loading, data, error } = state;
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div> 에러 발생 </div>;
-  if (!users) return null;
+  if (!data) return null;
 
   return (
     <>
       <ul>
-        {users.map((user) => (
+        {data.map((user) => (
           <li key={user.id}>
             {user.username} ({user.name})
           </li>
