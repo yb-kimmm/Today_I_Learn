09443bbe7ff6f329ba0cfa45e0887407d4154408
nan-yb <dydqls5757@gmail.com>
2022-04-10 19:03:09 +0900

    [study]-리덕스 미들웨어(redux-thunk , redux-logger)

---
+++ react/react-redux-middleware/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./modules";
+// import myLogger from "./middleware/myLogger";
+import logger from "redux-logger";
+
+// Redux 개발자도구 익스텐션 활용
+import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+);
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
