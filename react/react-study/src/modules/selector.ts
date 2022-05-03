553a773b838c051657de63b3a712afed5a89040d
nan-yb <dydqls5757@gmail.com>
2022-05-03 22:14:00 +0900

    [study]_SPA 개발 리액트 with 타입스크립트 10장_미완성

---
+++ react/react-study/src/modules/selector.ts
@@ -0,0 +1,23 @@
+import { createSelector } from "reselect";
+import { RootState } from "./index";
+
+const getAccessToken = (state : RootState) => state.auth.accessToken;
+
+const getMyInfo = (state : RootState) => state.auth.myInfo;
+
+export const getAuthorized = createSelector(
+  [getAccessToken , getMyInfo] ,
+  (accessToken , myInfo) => accessToken > 0 && !!myInfo
+)
+
+export const isAdmin = createSelector(
+  [getAccessToken , getMyInfo] ,
+  (isAuthorized , myInfo) => isAuthorized && !!myInfo && myInfo.authList[0].auth === "ROLE_ADMIN" 
+)
+
+export const isMember = createSelector(
+  [getAuthorized , getMyInfo] ,
+  (isAuthorized , myInfo ) => isAuthorized && !! myInfo && myInfo.authList[0].auth ==="ROLE_MEMBER"
+);
+
+
